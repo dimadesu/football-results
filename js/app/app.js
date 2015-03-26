b46188de55a3da5e4fad8e5ab35117cba62242ff
Dima Antonov <antonov.dmitriy@gmail.com>
2015-03-26 15:40:03 +0200

    Added interceptor for error and render them as angular ui bootstrap alert

--- js/app/app.js
+++ js/app/app.js
@@ -6,17 +6,35 @@
         'ui.router',
         'restangular',
         'angularCharts',
+        'ui.bootstrap',
         // App modules
+        'app.alerts',
         'app.group-results',
         'app.teams',
         'app.team-results'
-    ]).config(function ($urlRouterProvider, RestangularProvider) {
+    ]).config(function ($urlRouterProvider, RestangularProvider, $provide, $httpProvider) {
 
         // Handle unmatched url
         $urlRouterProvider.otherwise("/group-results/");
 
         RestangularProvider.setBaseUrl('http://worldcup.sfg.io');
         RestangularProvider.setFullResponse(true);
+
+        $provide.factory('httpInterceptor', function ($q) {
+            var interceptor = {
+                response: function (response) {
+                    return response || $q.when(response);
+                },
+                responseError: function (rejection) {
+                    interceptor.errors.push('Cannot load ' + rejection.config.url);
+                    return $q.reject(rejection);
+                },
+                errors: []
+            };
+            return interceptor;
+        });
+
+        $httpProvider.interceptors.push('httpInterceptor');
 
     });
 
